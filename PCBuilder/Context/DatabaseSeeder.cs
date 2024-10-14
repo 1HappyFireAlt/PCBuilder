a881fb17695951c0657d42715ecd1c23c53226f5
1HappyFire <abdi099q@gmail.com>
2024-10-14 12:09:12 +0100

    Database

---
+++ PCBuilder/Context/DatabaseSeeder.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PCBuilder.Model;
+
+namespace PCBuilder.Context
+{
+    public class DatabaseSeeder
+    {
+        private readonly DatabaseContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DatabaseSeeder(DatabaseContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public async Task Seed()
+        {
+            await _context.Database.MigrateAsync();
+
+            if (!_context.Users.Any())
+            {
+                await _roleManager.CreateAsync(new IdentityRole("Admin"));
+                await _roleManager.CreateAsync(new IdentityRole("Customer"));
+
+                var adminEmail = "admin@cheese.com";
+                var adminPassword = "Cheese123!";
+
+                var admin = new User
+                {
+                    UserName = adminEmail,
+                    Email = adminEmail,
+                };
+
+                await _userManager.CreateAsync(admin, adminPassword);
+                await _userManager.AddToRoleAsync(admin, "Admin");
+            }
+        }
+    }
+}
