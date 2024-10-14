f1b3751b8362752e54970536b11cb8a258278527
1HappyFire <abdi099q@gmail.com>
2024-10-14 10:36:12 +0100

    Database

--- PCBuilder/Context/DatabaseContext.cs
+++ PCBuilder/Context/DatabaseContext.cs
@@ -6,11 +6,16 @@ namespace PCBuilder.Context
 {
     public class DatabaseContext : IdentityDbContext<User>
     {
+        private IWebHostEnvironment _environment;
         public DbSet<User> Users { get; set; }
         public DbSet<Builds> Builds { get; set; }
         public DbSet<Component> Components { get; set; }
         public DbSet<Basket> Baskets { get; set; }
         public DbSet<Community> Communities { get; set; }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options, IWebHostEnvironment environment) : base(options)
+        {
+            _environment = environment;
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionbuilder)
         {
             var folder = Environment.SpecialFolder.MyDocuments;
