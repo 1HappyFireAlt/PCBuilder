83438542cffeb0ec436bf97b59bafb57f8092f6b
1HappyFire <abdi099q@gmail.com>
2024-10-22 13:09:15 +0100

    Fix the Shopping cart database

---
+++ PCBuilder/Model/OrderModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PCBuilder.Model
+{
+    public class OrderModel
+    {
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string CreditCardNumber { get; set; }
+    }
+}
