5bde087d0507ba24644e21c28054e407069a8c0b
1HappyFire <abdi099q@gmail.com>
2024-11-11 10:37:06 +0000

    Customise register

--- PCBuilder/Model/User.cs
+++ PCBuilder/Model/User.cs
@@ -8,13 +8,12 @@ namespace PCBuilder.Model
     {
         public string Name { get; set; }
         public string Gender { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
         public List<Order> Orders { get; set; } = [];
 
         public ICollection<Builds> Builds { get; set; }
         public ICollection<ShopBasket> Baskets { get; set; }
 
-        private DateTime _dateOfBirth;
         public bool TrySetDateOfBirth(DateTime dateOfBirth)
         {
             var age = DateTime.Today.Year - dateOfBirth.Year;
