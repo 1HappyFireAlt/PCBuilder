33b0f2ebcbe7e02daebb563d8e0e38a3dc328604
1HappyFire <abdi099q@gmail.com>
2024-11-13 10:40:47 +0000

    Register DateOfBirth

--- PCBuilder/Model/User.cs
+++ PCBuilder/Model/User.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -14,18 +15,5 @@ namespace PCBuilder.Model
         public ICollection<Builds> Builds { get; set; }
         public ICollection<ShopBasket> Baskets { get; set; }
 
-        public bool TrySetDateOfBirth(DateTime dateOfBirth)
-        {
-            var age = DateTime.Today.Year - dateOfBirth.Year;
-            if (dateOfBirth > DateTime.Today.AddYears(-age)) age--;
-
-            if (age >= 13)
-            {
-                DateOfBirth = dateOfBirth;
-                return true;
-            }
-
-            return false;
-        }
     }
 }
