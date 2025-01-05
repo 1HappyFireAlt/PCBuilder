1796eeece1d68260b674acc889fd5b05ead9f0ce
Fire Fire <fire099q@gmail.com>
2025-01-05 15:07:26 +0000

    Changes

--- PCBuilder/Model/ComponentCategory.cs
+++ PCBuilder/Model/ComponentCategory.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; }
         public string Category { get; set; }
         public string IconClass { get; set; }
+        public Component? SelectedComponent { get; set; }
         public List<Component> Options { get; set; } = new List<Component>();
     }
 }
